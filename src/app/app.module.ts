a3f4b317960c3c0f09a84278fa8530de785fab05
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-04-15 04:59:39 +0200

    feat: testing Prod Env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		DropdownModule,
+		FormsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
