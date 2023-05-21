387fc643ab92ab7db4dfdf764de9a0d1d621317e
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-05-22 00:46:27 +0300

    feat: :tada: add slider tics

--- src/app/modules/auth/layout/components/auth-slider/auth-slider.component.ts
+++ src/app/modules/auth/layout/components/auth-slider/auth-slider.component.ts
@@ -1,4 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    HostListener,
+    ViewChildren,
+    ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
     selector: 'app-auth-slider',
@@ -22,4 +27,18 @@ export class AuthSliderComponent {
             qoute: '“Lorem ipsum dolor sit amet consectetur. Nec aenean pellentesque est porta gravida aliquam sed.”',
         },
     ];
+
+    //TODO: handel skrew animation for tics elements
+
+    // @HostListener('document:mousemove', ['$event'])
+    // onMouseMove(e: MouseEvent) {
+    //     // select after and before elements on the .p-carousel dev
+    //     const tics = document.querySelectorAll('.ellipses');
+
+    //     (tics[0] as HTMLElement).style.transform = `skew(${e.clientX / 800}deg,
+    // 		${e.clientY / 150}deg), translateX(50%)`;
+
+    //     (tics[1] as HTMLElement).style.transform = `skew(${e.clientX / 800}deg,
+    // 		${e.clientY / 150}deg), translateX(-50%)`;
+    // }
 }
