9fa513fb5257c7031200c1a1f9ed579f3a978687
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-05-26 01:32:12 +0300

    feat(auth): sign up component

--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -42,7 +42,7 @@ const routes: Routes = [
             },
             {
                 path: '**',
-                redirectTo: 'sign-in',
+                redirectTo: 'sign-up',
                 pathMatch: 'full',
             },
         ],
