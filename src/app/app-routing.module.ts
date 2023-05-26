902aa98e2c7f7df5094de9da8356a6319048652b
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-05-26 12:58:10 +0300

    fix testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,11 @@
-import { NgModule, inject } from '@angular/core';
-import { RouterModule, Routes, CanLoad, Router } from '@angular/router';
-import { AuthenticationService } from '@core/services/authentication.service';
-import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-    },
-    {
-        path: 'home',
-        loadComponent: () =>
-            import('./landing-page/home/home.component').then(
-                (m) => m.HomeComponent
-            ),
-    },
-    {
-        path: 'auth',
-        // !! Add Guards to redirect Authenticated users to home 👍
-        canLoad: [
-            () => {
-                inject(AuthenticationService).isAuthenticated.subscribe(
-                    (isAuthenticated) => {
-                        if (isAuthenticated) {
-                            inject(Router).navigate(['home']);
-                            return false;
-                        }
-                        return true;
-                    }
-                );
-            },
-        ],
         loadChildren: () =>
-            import('./modules/auth/auth.module').then((m) => m.AuthModule),
-    },
-    {
-        path: '404',
-        component: NotFoundComponent,
-    },
-    {
-        path: '**',
-        redirectTo: '404',
-        pathMatch: 'full',
+            import('./modules/modules.module').then((m) => m.ModulesModule),
     },
 ];
 
