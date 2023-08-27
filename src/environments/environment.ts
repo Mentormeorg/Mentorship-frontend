5d72403d71c197bd6809840049b9d5e289dc22e6
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-08-27 03:27:04 +0300

    refactor(global-style): :rocket: global refactor Part 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     // ! Add base url here / backend localhost server
     // baseUrl: 'http://api-testing.mentorchief.net/api/v1',
-    baseUrl: 'http://localhost:3003/api/v1',
+    baseUrl: 'http://localhost:3000/api/v1',
     // baseUrl: 'http://api.mentorchief.net/api/v1',
     title: 'MentorChief',
 };
