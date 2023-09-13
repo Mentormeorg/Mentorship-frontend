6830e2068de1412699bfa908bbc75a187eb8e217
Mahmoudibrahim03 <mahmoud0ibrahim0@gmail.com>
2023-09-13 22:08:40 +0300

    feat(global): add firebase deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,17 @@ import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './landing-page/home/home.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -45,8 +56,20 @@ export function HttpLoaderFactory(http: HttpClient) {
                 deps: [HttpClient],
             },
         }),
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAnalytics(() => getAnalytics()),
+        provideAuth(() => getAuth()),
+        provideDatabase(() => getDatabase()),
+        provideFirestore(() => getFirestore()),
+        provideFunctions(() => getFunctions()),
+        provideMessaging(() => getMessaging()),
+        providePerformance(() => getPerformance()),
+        provideRemoteConfig(() => getRemoteConfig()),
+        provideStorage(() => getStorage()),
     ],
-    providers: [],
+    providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'mentorchief-383902',
+    appId: '1:263225123218:web:bddd2d697a489b2e62a14b',
+    storageBucket: 'mentorchief-383902.appspot.com',
+    apiKey: 'AIzaSyBFIkzqnK-UukVZ2NoGI8gnAil4UZBgZy4',
+    authDomain: 'mentorchief-383902.firebaseapp.com',
+    messagingSenderId: '263225123218',
+    measurementId: 'G-DRJXKHVEVJ',
+  },
     production: true,
     // ! Add base url here / backend localhost server
     baseUrl: 'https://api-stagin.mentorchief.net/api/v1',
